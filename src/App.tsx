6283882404b69fa6bacc5b01f471efda4aa42f0f
Timofei Pendus <ptimich@gmail.com>
2024-02-29 00:13:49 +0200

    (chore): Add prettier formatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <main>
-    <h1>Google Books Viewer</h1>
+      <h1>Google Books Viewer</h1>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
